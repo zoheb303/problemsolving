ddc15cfffbebd867007ecc84f7d6b042a7684c4f
root <root@localhost.localdomain>
2020-08-09 19:21:45 +0530

    triplet sum =0 problem

---
+++ array/tripletWithZeroSum/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func tripletSum(a []int) [][]int {
+
+	//res := make([]int, 3)
+	// numRows := 5
+
+	var res2d [][]int
+	// // Initialize a five length slice of empty slices
+	// grid := make([][]int, numRows)
+
+	// // print out slices
+	// fmt.Println(grid)
+
+	// // Initialize the 5 empty slices
+	// for i := 0; i < numRows; i++ {
+	//    grid[i] = make([]int, 4)
+	// }
+
+	// // print out slices
+	// fmt.Println(grid)
+	sort.Ints(a)
+	fmt.Println(a)
+	n := len(a) - 1
+	for i := 0; i <= n-2; i++ {
+		if i > 0 && a[i] == a[i-1] {
+			continue
+		}
+
+		left := i + 1
+		right := n
+		fmt.Println(left, i, right)
+		for left < right {
+			var res []int
+
+			if a[left]+a[i]+a[right] < 0 {
+
+				left++
+
+			} else if a[left]+a[i]+a[right] > 0 {
+
+				right--
+			} else if a[left]+a[i]+a[right] == 0 {
+
+				res = append(res, a[i], a[left], a[right])
+
+				left++
+				right--
+				for left < right && a[left] == a[left-1] {
+					left++
+				}
+				for left < right && a[right] == a[right+1] {
+					right--
+				}
+			}
+			if len(res) != 0 {
+				res2d = append(res2d, res)
+			}
+		}
+
+	}
+	return res2d
+}
+
+func main() {
+
+	var arr = []int{-2, 0, 0, 2, 2}
+	//var arr = []int{0, 0, 0}
+
+	sort.Ints(arr)
+
+	a := tripletSum(arr)
+
+	fmt.Println(a)
+
+}
